c536fc7ecb78839fae493654b55f5dc8bdb6c7b6
Szymon Koper <sakydpozrux@gmail.com>
2013-12-20 03:18:20 +0100

    More help and run informations printed

--- help.c
+++ help.c
@@ -23,15 +23,27 @@ void show_usage() {
   printf("\t-e | --consumers-wait <value>      Set waiting time for consumer process after consuming\n");
   printf("\t-l | --limit <value>               Set limit of total products to produce\n");
   printf("\t-h | --help                        Display this help\n");
+  printf("\n");
+  printf("Default values:\n");
+  printf("\t--producents-number\t2\n");
+  printf("\t--consumers-number \t4\n");
+  printf("\t--producents-wait  \t1\n");
+  printf("\t--consumers-wait   \t2\n"); 
+  printf("\t--limit            \t10\n");
+
+  printf("\n");
+  printf("Author:   Szymon Koper -- sakydpozrux\n");
+  printf("Date:     18.12.2013\n");
 }
 
 void show_run_info() {
-  printf(
-    "\n"
-    "Run informations: \n"
-    "%d\t- number of consumers\n"
-    "%d\t- waiting time for producer\n"
-    "%d\t- waiting time for consumer\n"
-    "\n"
-    , shared_mem->consumers.number, shared_mem->producers.sleep_time, shared_mem->consumers.sleep_time);
+  printf("\n");
+  printf("Run informations: \n");
+  printf("%u\t- number of producers\n",       shared_mem->producers.number);
+  printf("%u\t- number of consumers\n",       shared_mem->consumers.number);
+  printf("%u\t- waiting time for producer\n", shared_mem->producers.sleep_time);
+  printf("%u\t- waiting time for consumer\n", shared_mem->consumers.sleep_time);
+  printf("%u\t- limit of total products\n",   shared_mem->products_limit);
+  printf("Run -h or --help for more informations.\n");
+  printf("\n");
 }
